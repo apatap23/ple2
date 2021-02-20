26da8b22d5648a720a9b0b66c68331e7c54a60fa
apatap23 <77858823+apatap23@users.noreply.github.com>
2021-02-19 21:47:52 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,12 @@ backpack = ["pizza", "broccoli","frozen custard", "apple crisp", "kale chips"]
 
 backpack[:]= [item for item in backpack if item in healthy]
 print(backpack)
+
+
+def __iter__(self):
+    cursor = 0
+    while cursor < len(self):
+        yield self._items [(self._front + cursor) % len(self._items)]
+        cursor += 1 
+
+        
