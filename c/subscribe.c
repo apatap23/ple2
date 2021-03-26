53b323c59c54ee6156022943adf86b509ab3697a
Praise Apata <77858823+apatap23@users.noreply.github.com>
2021-03-25 21:16:43 -0400

    Update subscribe.c

--- c/subscribe.c
+++ c/subscribe.c
@@ -3,11 +3,11 @@
 int main()
 {
 
-	int x = 10;
-	int y = x/2;
-	
-	printf("%i\n",y);
-	return 0;
+        int x = 10;
+        int y = x/2;
+
+        printf("The magic number is: %i\n",y);
+        return 0;
 
 
 }
