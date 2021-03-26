a44eb46f59b008c879acdb4a839e2b53da7d5603
Praise Apata <77858823+apatap23@users.noreply.github.com>
2021-03-25 21:46:45 -0400

    Update helloworld.c

--- c/helloworld.c
+++ c/helloworld.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int main(){
-  printf("Hello World!\n");
+int main()
+{
+        char name[31];
+        printf("What's your name: ");
+        scanf("%s",&name);
+        printf("Hello %s!\n", name);
+        return 0;
+
 }
