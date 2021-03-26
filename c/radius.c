50381c00c1b3db9916c777fcebf35c25b2f82420
Praise Apata <77858823+apatap23@users.noreply.github.com>
2021-03-25 21:25:33 -0400

    Update and rename subscribe.c to radius.c

---
+++ c/radius.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+        int radius;
+        printf("Please enter a radius: ");
+        scanf("%i", &radius);
+        int y = radius / 2;
+        printf("The magic number is: %i\n",y);
+        return 0;
+
+}
